c0a94e7e141ede6fd5e23ff0274ab2fa69304f7f
Adam Renberg <tgwizard@gmail.com>
2015-08-22 16:57:04 +0200

    Add tox

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ from setuptools import setup, find_packages
 setup(
     name='claw',
     version='1.0.0rc4',
-    description="Library to extract message quotations and signatures.",
-    long_description=open(os.path.join(os.path.dirname(__file__), "README.md")).read(),
+    description='Library to extract message quotations and signatures.',
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
     author='Adam Renberg',
     author_email='tgwizard@gmail.com',
     url='https://github.com/tgwizard/claw',
@@ -15,17 +15,18 @@ setup(
     packages=find_packages(exclude=['tests']),
     zip_safe=True,
     install_requires=[
-        "lxml==2.3.3",
-        "regex==0.1.20110315",
-        "html2text",
-        "setuptools>=17.1",
+        'lxml==2.3.3',
+        'regex==0.1.20110315',
+        'html2text',
+        'setuptools>=17.1',
     ],
     extras_require={
         'tests': [
-            "nose==1.2.1",
-            "mock",
-            "coverage",
-            "flanker",
+            'nose==1.2.1',
+            'mock',
+            'coverage',
+            'flanker',
+            'tox',
         ],
         'dev': [
             'twine'
